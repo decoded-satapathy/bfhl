6d090537ec0af27282785cba9cce6cc2bd715aae
Om Satapathy <omsatapathy05@gmail.com>
2025-02-21 17:33:06 +0530

    project init

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -195,7 +195,7 @@ function App() {
       <div className="max-w-2xl mx-auto">
         <div className="bg-white p-8 rounded-lg shadow-lg">
           <h1 className="text-2xl font-bold text-gray-900 mb-8">
-            Data Processing Interface
+            Submission for <strong>BFHL</strong>
           </h1>
 
           <form onSubmit={handleSubmit} className="space-y-6">
